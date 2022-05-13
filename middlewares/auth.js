1b4726972f142df0e4b9a23536fbc368024a4a99
Anastasia Korniichuk <81706073+ms-anastasia@users.noreply.github.com>
2022-05-13 19:33:22 +0300

    small fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-const {Unauthorized} = require("http-errors");
+const { Unauthorized } = require("http-errors");
 const jwt = require("jsonwebtoken");
 
 const {User} = require('../models');
@@ -9,13 +9,13 @@ const auth = async(req, res, next) => {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ")
     if(bearer !== "Bearer") {
-        throw new Unauthorized("Not authorized")
+        throw new Unauthorized("Not authorized");
     }
     try {
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if(!user || !user.token){
-            throw new Unauthorized("Not authorized")
+        if(!user || !user.token) {
+            throw new Unauthorized("Not authorized");
         }
         req.user = user;
         next();
